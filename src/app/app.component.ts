9c8183c13957b2bd358769ef8fa30e219f8d15ab
Julien <julien.blase05@gmail.com>
2024-02-28 15:43:27 +0100

    Ajout de route pour l'application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +9,20 @@ import { HomeComponent } from './home/home.component';
   // imports: [RouterOutlet],
   imports: [
     HomeComponent,
+    RouterModule,
   ],
-  template: `<main>
-  <header class="brand-name">
-    <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-  </header>
-  <section class="content">
-    <app-home></app-home>
-  </section>
-</main>`,
+  template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
